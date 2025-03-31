7c3cb0223bc1704b9991c13aa3ad12f63acc2052
lenniliu <lennil@metropolia.fi>
2025-03-31 15:57:28 +0300

    UI layout Excersice screen

--- app/src/main/java/com/example/mobilepet/screens/exerciseScreen.kt
+++ app/src/main/java/com/example/mobilepet/screens/exerciseScreen.kt
@@ -1,10 +1,65 @@
 package com.example.mobilepet.screens
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.mobilepet.navigation.BottomNavigationBar
 
 @Composable
 fun ExerciseScreen(navController: NavController) {
     Text("Exercise Screen")
+
+    Scaffold (
+        bottomBar = { BottomNavigationBar(navController) },
+        containerColor = MaterialTheme.colorScheme.background
+    ){ innerPadding ->
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp, top = 24.dp, bottom = 16.dp)
+                    .border(
+                        width = 2.dp,
+                        color = MaterialTheme.colorScheme.outline,
+                        shape = MaterialTheme.shapes.medium
+                    )
+            ){
+                Text(
+                    text = "Status Bar",
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .align(Alignment.CenterHorizontally)
+                )
+            }
+
+            //Tästä alkaa kartan ihmettely
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ExerciseScreenPreview() {
+    val navController = rememberNavController()
+    ExerciseScreen(navController)
 }
